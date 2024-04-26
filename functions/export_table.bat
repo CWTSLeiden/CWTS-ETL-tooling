d368b2dbfc8667a56dadd4eb5f33fd2528fecba0
Bram van den Boomen <bram.boomen@gmail.com>
2024-04-26 11:31:22 +0200

    Add function csv_analyze_data
    - integrate in export_table

--- functions/export_table.bat
+++ functions/export_table.bat
@@ -45,6 +45,12 @@ if "%export_table_include_types%" == "true" (
         "-log_folder %log_folder%" ^
         "%verbose_arg%"
 )
+if "%export_table_include_types%" == "analyze" (
+    set csv_analyzer_output_columns='%table_name%' as table_name, column_name, mssql_type as data_type, max_length, is_nullable
+    call %functions_folder%\csv_analyze_data.bat ^
+        "%table_query_file%" ^
+        "%types_file%"
+)
 
 endlocal
 goto:eof
@@ -81,7 +87,7 @@ if exist %table_or_file% (
     set export_table_include_types=false
     set table_query_file=%table_or_file%
     for %%f in (%table_or_file%) do set table_name=%%~nf
-    set export_table_include_types=false
+    set export_table_include_types=analyze
 ) else (
     set table_name=%table_or_file%
     set table_query_file=%functions_folder%\export_table\export_table.sql
